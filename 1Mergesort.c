cbc1e0efda044fa1d030e15a4ee3cf5150dde0c8
sksvk2000 <52601700+sksvk2000@users.noreply.github.com>
2020-06-01 11:01:37 +0530

    Update 1Mergesort.c

--- 1Mergesort.c
+++ 1Mergesort.c
@@ -1,66 +1,82 @@
 #include<stdio.h>
-#include<stdlib.h>
-int b[10], count = 0;
-void merge(int a[], int low, int mid, int high){
-        int i = low, j = mid+1, k = low;
-        while(i<=mid && j<=high){
-                if(a[i] < a[j])
-                        b[k++] = a[i++];
-                else
-                        b[k++] = a[j++];     
-                count++;
-        }
-        while(i<=mid){          //if remaining elements in left array directly copy to b because already sorted
-                b[k++] = a[i++];
-                count++;
-        }
-        while(j<=high){         //if remaining elements in right array directly copy to b because already sorted
-                b[k++] = a[j++];
-                count++;
-        }
-        for(i = low; i<k; i++)  //copy all sorted elements to the original array 
-                a[i] = b[i];
+int b[600],count=0;
+void merge(int a[],int low,int mid,int high){
+	int i=low,j=mid+1,k=low;
+
+	while(i<=mid && j<=high){
+		count++;
+		if(a[i]>a[j])
+			b[k++]=a[j++];
+		else
+			b[k++]=a[i++];
+	}
+
+	for(;i<=mid;i++){      //if remaining elements in left array directly copy to b because already sorted
+		count++;   
+		b[k++]=a[i];
+	}
+
+	for(;j<=high;j++){    //if remaining elements in right array directly copy to b because already sorted
+		count++;
+		b[k++]=a[j];
+	}
+
+	for(i=0;i<=high;i++)  //copy all sorted elements to the original array 
+		a[i]=b[i];
 }
 
-void mergeSort(int a[], int low, int high){
-        int mid;
-
-        if(low<high){
-                count++;
-                mid = (low + high)/2;
-                mergeSort(a, low, mid);    //recursively sort the left half
-                mergeSort(a, mid+1, high); //recursively sort the right half
-                merge(a, low, mid, high);  //merge the two sorted halves
-        }
+void mergeSort(int a[],int low,int high){
+	int mid;
+	if(low<high){
+		count++;
+		mid=(low+high)/2;
+		mergeSort(a,low,mid);    //recursively sort the left half
+		mergeSort(a,mid+1,high); //recursively sort the right half
+		merge(a,low,mid,high);   //merge the two sorted halves
+	}
 }
 
-int main(){
-        int i,j, a[8] = {9,3,7,5,6,4,8,2}, A[600], B[600], C[600];
-        printf("Array::\n");
-        for(i = 0; i<8; i++)
-                printf("%d ", a[i]);
-        printf("\n");
-        mergeSort(a, 0, 7);
-        printf("\nArray::\n");
-        for(i = 0; i<8; i++)
-                printf("%d ",a[i]);
-        printf("\nCount = %d\n", count);
-        for(i = 15; i<520; i+=500){
-                for(j = 0; j <= i; j++){
-                        A[j] = j;
-                        B[j] = i-j;
-                        C[j] = rand()%i;
-                }
-                count = 0;
-                mergeSort(A, 0, i-1);
-                printf("\nCount for ASC = %d\n", count);
-                count = 0;
-                mergeSort(B, 0, i-1);
-                printf("\nCount for DESC = %d\n", count);
-                count = 0;
-                mergeSort(C, 0, i-1);
-                printf("\nCount for RAND = %d\n", count);
-                printf("\n\n");
-        }
-        return 0;
+void main(){
+	int a[600],b[600],c[600],i,j,n;
+
+	printf("\nEnter the size of array :");
+	scanf("%d",&n);
+
+	printf("\nEnter the array elements:");
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+
+	mergeSort(a,0,n-1);
+
+	printf("\nThe Sorted array is :");
+
+	for(i=0;i<n;i++)
+		printf("\t%d",a[i]);
+
+	printf("\nThe count is : %d",count);
+
+	printf("\nTesting algorithm for Ascending, Descending and Random Arrays\n");
+	printf("Size \tAscen \tDesc \tRandom\n");
+
+	for(i=16;i<=600;i=i*2){
+		for(j=1;j<=i;j++){
+			a[j]=j;
+			b[j]=i-j;
+			c[j]=rand()%i;
+		}
+
+		count=0;
+		mergeSort(a,1,i);
+		printf("\n%d\t%d",i,count);
+
+		count=0;
+                mergeSort(b,1,i);
+                printf("\t%d",count);
+
+		count=0;
+                mergeSort(c,1,i);
+                printf("\t%d",count);
+	}
+	
 }
+
