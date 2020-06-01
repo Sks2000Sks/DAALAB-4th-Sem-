8aa5eb783dcd86f817a5389e474d1ad9dd2ebf47
sksvk2000 <52601700+sksvk2000@users.noreply.github.com>
2020-06-01 22:52:17 +0530

    Update 8Prims.c

--- 8Prims.c
+++ 8Prims.c
@@ -22,21 +22,21 @@ int printMST(int parent[], int graph[V][V])
 void primMST(int graph[V][V])
 {
 	int parent[V];
-	int key[V]; //weight
+	int weight[V]; 
 	bool mstSet[V];
 
 	for (int i = 0; i < V; i++)
-		key[i] = INT_MAX, mstSet[i] = false;
+		weight[i] = INT_MAX, mstSet[i] = false;
   
-	key[0] = 0;
+	weight[0] = 0;
 	parent[0] = -1; 
 	for (int count = 0; count < V - 1; count++) {
 		int u = minKey(key, mstSet);
 		mstSet[u] = true;
 		for (int v = 0; v < V; v++)
 		  //adjacent and not visited and graph[u][v] is smaller than key[v]
-			if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
-				parent[v] = u, key[v] = graph[u][v];
+			if (graph[u][v] && mstSet[v] == false && graph[u][v] < weight[v])
+				parent[v] = u, weight[v] = graph[u][v];
 	}
 	printMST(parent, graph);
 }
